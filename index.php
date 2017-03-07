1ded93314def5eac62ae0cff08318c0586d5dfa1
alexisbarnes <alexis.barnes06@gmail.com>
2017-03-06 22:35:31 -0500

    Added icons. They are not showing up on page.

--- index.php
+++ index.php
@@ -12,48 +12,58 @@
 
 ?>
 
+<!--CREATED MESSAGE-->
 <?php if (array_key_exists('created', $_GET)) : ?>
 <div class="alert alert-info">
   <p><strong>Contact created!</strong> Your contact was successfully created.</p>
 </div>
 <?php endif; ?>
 
+<!--DELETED MESSAGE-->
 <?php if (array_key_exists('deleted', $_GET)) : ?>
 <div class="alert alert-danger">
   <p><strong>Contact Deleted!</strong> The contact was removed.</p>
 </div>
 <?php endif; ?>
 
+
 <h1>All Contacts <span class="text-muted">(<?= count($contacts); ?>)</span></h1>
-<table class="table table-hover">
-  <thead>
-    <th>ID</th>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>Title</th>
-    <th>Address</th>
-    <th>City</th>
-    <th>State</th>
-    <th>Zip Code</th>
-    <th>Phone Number</th>
-    <th>Notes</th>
-  </thead>
-  <tbody>
-    <?php foreach($contacts as $contact) : ?>
-    <tr>
-      <td><a href="/edit.php?id=<?= $contact['id']; ?>"><?= $contact['id']; ?></a></td>
-      <td><?= $contact['first']; ?></td>
-      <td><?= $contact['last']; ?></td>
-      <td><?= $contact['title']; ?></td>
-      <td><?= $contact['address']; ?></td>
-      <td><?= $contact['city']; ?></td>
-      <td><?= $contact['state']; ?></td>
-      <td><?= $contact['zip']; ?></td>
-      <td><?= $contact['phone']; ?></td>
-      <td><?= $contact['notes']; ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+  <!-- Table -->
+  <table class="table table-hover">
+    <thead class="thead-inverse">
+      <th>ID</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Title</th>
+      <th>Address</th>
+      <th>City</th>
+      <th>State</th>
+      <th>Zip Code</th>
+      <th>Phone Number</th>
+      <th>Notes</th>
+    </thead>
+    <tbody>
+      <?php foreach($contacts as $contact) : ?>
+      <tr>
+        <td><a href="/edit.php?id=<?= $contact['id']; ?>"><?= $contact['id']; ?></a></td>
+        <td><?= $contact['first']; ?></td>
+        <td><?= $contact['last']; ?></td>
+        <td><?= $contact['title']; ?></td>
+        <td><?= $contact['address']; ?></td>
+        <td><?= $contact['city']; ?></td>
+        <td><?= $contact['state']; ?></td>
+        <td><?= $contact['zip']; ?></td>
+        <td><?= $contact['phone']; ?></td>
+        <td><?= $contact['notes']; ?></td>
+        <td>
+          <a href="/edit.php?id=<?= $contact['id']; ?>"><img src="icons/pencil.png" alt="edit"></a>
+          <a href="/delete.php?id<?= $contact['id']; ?>"><img src="icons/x.png" alt="delete"></a>
+        </td>
+      </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+
+
 
 <?php include 'footer.php'; ?>
